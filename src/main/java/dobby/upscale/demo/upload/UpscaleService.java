ea0883a77fb409f74fb90165ed8c63c1bc86ea63
KSYeon103 <70624170+KSYeon103@users.noreply.github.com>
2021-09-04 17:48:09 +0900

    결과물 result 폴더에서 압축 후 다운로드 구현

--- src/main/java/dobby/upscale/demo/upload/UpscaleService.java
+++ src/main/java/dobby/upscale/demo/upload/UpscaleService.java
@@ -10,7 +10,7 @@ import java.util.concurrent.Executors;
 
 @Service("upscaleService")
 public class UpscaleService {
-
+    // return 값은 result 폴더의 경로
     public String doConvert(String radio) {
 
         try {
@@ -24,27 +24,38 @@ public class UpscaleService {
 
             ProcessBuilder builder = new ProcessBuilder();
             if (isWindows) {
-                // 사용할 모델의 루트를 제외한 나머지
-                String baseRoute = "C:\\ProgramData\\Anaconda3\\Scripts\\activate.bat " +
-                        "C:\\ProgramData\\Anaconda3 " +
-                        "&& cd C:\\src\\git\\Real-ESRGAN-210902 " +
-                        "&& python inference_realesrgan.py --model_path";
+                // 아나콘다 실행 명령어
+                String anaconda = "C:\\ProgramData\\Anaconda3\\Scripts\\activate.bat " +
+                        "C:\\ProgramData\\Anaconda3 ";
+                // 사용할 파이썬 파일의 루트
+                String pyRoute = null;
                 // 사용할 모델의 루트
                 String modelRoute = null;
+                // 기타 옵션
+                String opthons = null;
+
                 switch(radio) {
                     case "basic":
-                        modelRoute = " C:\\src\\git\\Real-ESRGAN-210902\\experiments\\pretrained_models\\RealESRGAN_x4plus.pth";
+                        // Real-Esrgan
+                        pyRoute = "&& cd C:\\src\\git\\Real-ESRGAN-210902 " +
+                                "&& python inference_realesrgan.py ";
+                        modelRoute = "--model_path C:\\src\\git\\Real-ESRGAN-210902\\experiments\\pretrained_models\\RealESRGAN_x4plus.pth ";
+                        opthons = "--input inputs ";
                         break;
                     case "image":
+                        pyRoute = "";
                         modelRoute = "";
+                        opthons = "";
                         break;
                     case "photo":
+                        pyRoute = "";
                         modelRoute = "";
+                        opthons = "";
                         break;
                     default:
                         throw new IllegalStateException("Unexpected value: " + radio);
                 }
-                builder.command("cmd.exe", "/c", baseRoute+modelRoute+" --input inputs --face_enhance");
+                builder.command("cmd.exe", "/c", anaconda+pyRoute+modelRoute+opthons);
             } else {
                 builder.command("python", "/Users/psy/study/upscale_web/src/main/resources/python/test.py");
             }
@@ -63,10 +74,15 @@ public class UpscaleService {
             process.destroy();
             tesk1.shutdownNow();
 
-            return "ok";
+            return "C:/src/git/Real-ESRGAN-210902/results/";
+
         } catch (IOException | InterruptedException e) {
             return "fail";
         }
 
     }
+
+
+
+
 }
